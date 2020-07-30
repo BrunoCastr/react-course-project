a67cd6b288eeef8e1b93c53ad5f0e175a9879f71
Bruno <bncsotc@gmail.com>
2020-07-30 14:53:45 -0300

    JSX Expressions

--- indecision-app/public/scripts/app.js
+++ indecision-app/public/scripts/app.js
@@ -3,18 +3,23 @@
 console.log("App.js is running");
 
 // JSX - JavaScript XML
+var application = {
+    title: 'Askaban',
+    subtitle: "You don't know nothing!"
+};
+
 var template = React.createElement(
     "div",
     null,
     React.createElement(
         "h1",
         null,
-        "Indecision App"
+        application.title
     ),
     React.createElement(
         "p",
         null,
-        "This is some info"
+        application.subtitle
     ),
     React.createElement(
         "ol",
@@ -32,27 +37,37 @@ var template = React.createElement(
     )
 );
 
+var user = {
+    name: 'Bruno',
+    age: 23,
+    location: 'Brazil'
+};
+var userName = 'Deko';
+var userAge = 23;
+var userLocation = 'Brazil';
 var templateTwo = React.createElement(
     "div",
     null,
     React.createElement(
         "h1",
         null,
-        "Bruno Lima"
+        user.name
     ),
     React.createElement(
         "p",
         null,
-        "Age: 23"
+        "Age: ",
+        user.age
     ),
     React.createElement(
         "p",
         null,
-        "Location: Brazil"
+        "Location: ",
+        user.location
     )
 );
 
 //var template = React.createElement("p", {id: "id"}, "This is JSX from app.js!");
 var appRoot = document.getElementById('app');
 
-ReactDOM.render(templateTwo, appRoot);
+ReactDOM.render(template, appRoot);
--- indecision-app/src/app.js
+++ indecision-app/src/app.js
@@ -1,10 +1,15 @@
 console.log("App.js is running")
 
 // JSX - JavaScript XML
+var application = {
+    title: 'Askaban',
+    subtitle: "You don't know nothing!"
+}
+
 var template = (
     <div>
-        <h1>Indecision App</h1>
-        <p>This is some info</p>
+        <h1>{application.title}</h1>
+        <p>{application.subtitle}</p>
         <ol>
             <li>Item one</li>
             <li>Item two</li>
@@ -12,15 +17,23 @@ var template = (
     </div>
 );
 
+var user = {
+    name: 'Bruno',
+    age: 23,
+    location: 'Brazil'
+}
+var userName = 'Deko';
+var userAge = 23;
+var userLocation = 'Brazil';
 var templateTwo = (
     <div>
-        <h1>Bruno Lima</h1>
-        <p>Age: 23</p>
-        <p>Location: Brazil</p>
+        <h1>{user.name}</h1>
+        <p>Age: {user.age}</p>
+        <p>Location: {user.location}</p>
     </div>
 );
 
 //var template = React.createElement("p", {id: "id"}, "This is JSX from app.js!");
 var appRoot = document.getElementById('app');
 
-ReactDOM.render(templateTwo, appRoot);
+ReactDOM.render(template, appRoot);
