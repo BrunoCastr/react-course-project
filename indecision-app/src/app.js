81c1e1ed86315635ce1bb6cb3e2eb5acee2ef947
Bruno <bncsotc@gmail.com>
2020-08-05 16:18:13 -0300

    Method Bingind

--- indecision-app/src/app.js
+++ indecision-app/src/app.js
@@ -1,3 +1,15 @@
+const obj = {
+    name: 'Vikram',
+    getName(){
+        return this.name;
+    }
+};
+
+const getName = obj.getName.bind(obj);
+
+
+console.log(getName());
+
 class IndecisionApp extends React.Component {
     render(){
         const title = "Indecision";
@@ -40,8 +52,13 @@ class Action extends React.Component {
 }
 
 class Options extends React.Component{
+    constructor (props){
+        super(props);
+        this.handleRemoveAll = this.handleRemoveAll.bind(this);
+    }
     handleRemoveAll(){
-        alert('handleRemoveAll')
+        console.log(this.props.options);
+        //alert('handleRemoveAll')
     }
     render(){
         return(
